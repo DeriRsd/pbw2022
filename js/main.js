9aa03347d13d54e9fbe5beb949e98d78c08d9c5a
DeriRsd <cepderirosadi@gmail.com>
2023-01-12 08:04:31 +0700

    Project UAS

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
-var removebtn = document.getElementsByClassName('btn-danger');
-for(i = 0 ; i < removebtn.length ; i++){
-    removebtn[i].addEventListener('click' , removebtns)
+
+var addTocart = document.getElementsByClassName("btn-secondary");
+for (i = 0; i < addTocart.length; i++) {
+  addTocart[i].addEventListener("click", addTocarts);
 }
-function removebtns(event){
-    var e = event.target;
-    var parents = e.parentElement.parentElement.remove()
-    //console.log(parents)
+function addTocarts(event) {
+  var addToCart = event.target;
+  var addTocartEl = addToCart.parentElement;
+  //console.log(addTocartEl)
+  var titlename = addTocartEl.children[0].innerText;
+  var text = addTocartEl.children[1].innerText;
+  addTocartupdate(titlename, text);
 }
-var addTocart = document.getElementsByClassName('btn-secondary');
-for(i = 0 ; i < addTocart.length ; i++){
-    addTocart[i].addEventListener('click' , addTocarts)
-}
-function addTocarts(event){
-    var addToCart = event.target;
-    var addTocartEl = addToCart.parentElement
-    //console.log(addTocartEl)
-    var titlename = addTocartEl.children[0].innerText;
-    var text = addTocartEl.children[1].innerText;
-    addTocartupdate(titlename , text)
-}
-var tbody = document.getElementsByTagName('tbody')[0]
-function addTocartupdate( titlename , text){
-    var createElement = document.createElement('tr');
-    createElement.innerHTML = `<td class="py-5"><h5 class="left2">${titlename}</h5></td>
+var tbody = document.getElementsByTagName("tbody")[0];
+function addTocartupdate(titlename, text) {
+  var createElement = document.createElement("tr");
+  createElement.innerHTML = `<td class="py-5"><h5 class="left2">${titlename}</h5></td>
     <td class="py-5"><h5 class="left">${text}</h5></td>
-    <td class="py-5"><button class="btn btn-danger" onclick="myFunction2()">Remove</button></td>`
-    tbody.append(createElement);
+    <td class="py-5"><button class="btn btn-danger" onclick="deletePost(this)">Remove</button></td>`;
+  tbody.append(createElement);
 }
+
+// testing
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
